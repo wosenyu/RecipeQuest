ab84f3f553eade0f20a17573da1adab9af440a9c
Wosen <wosen.yu72@gmail.com>
2023-10-26 00:55:23 -0400

    Update SignUp.js

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -11,7 +11,7 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Navbar from './Navbar';
+import Navbar from './navbar';
 
 function Copyright(props) {
     return (
