d3fab7f54bb28c17ee58bcd6f61397070bc64b74
Wosen <wosen.yu72@gmail.com>
2023-10-26 02:24:38 -0400

    Update EdamamApi.js

--- client/src/EdamamApi/EdamamApi.js
+++ client/src/EdamamApi/EdamamApi.js
@@ -17,7 +17,7 @@ export default function EdamamApi() {
 
     const handleSearch = () => {
         // Fetch data from the Edamam API with the search query
-        axios.get(`http://localhost:3001/api/edamam-recipes?q=${searchQuery}&from=0&to=4`)
+        axios.get(`https://recipe-quest-backend-17dg9ai0s-wosenyu.vercel.app/api/edamam-recipes?q=${searchQuery}&from=0&to=4`)
             .then((response) => {
                 const data = response.data.hits;
                 console.log(data); // Log the data received from the API
